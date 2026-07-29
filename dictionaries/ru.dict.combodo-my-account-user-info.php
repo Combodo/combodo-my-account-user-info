c45c7007d30ad67d77e8eec6ff2d2f6ae2f51946
Konstantin Nikulin <apollo2k4@narod.ru>
2026-07-29 14:10:30 +0300

    add RU dict

---
+++ dictionaries/ru.dict.combodo-my-account-user-info.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2024 Combodo SAS
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'MyAccount:UserInfo:Tab:Title' => 'Информация пользователя',
+	'MyAccount:SubTitle:user' => 'Мой пользователь',
+	'MyAccount:SubTitle:contact' => 'Мой контакт',
+));
